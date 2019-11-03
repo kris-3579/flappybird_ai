c30aa536cf9e188c331c207076d027234ffc9c5b
kris-3579 <52053880+kris-3579@users.noreply.github.com>
2019-11-02 23:07:31 -0400

    Update flappy_ai_implement.py

--- flappy_ai_implement.py
+++ flappy_ai_implement.py
@@ -12,23 +12,23 @@ import flappy_ai
 def preprocess_images(images):
     if images.shape[0] < 4:
         # single image
-        x_t = images[0]
-        x_t = resize(x_t, (80, 80))
-        x_t = x_t.astype("float")
-        x_t /= 255.0
-        s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
+        img = images[0]
+        img = resize(img, (80, 80))
+        img = img.astype("float")
+        img /= 255.0
+        new_images = np.stack((img, img, img, img), axis=2)
     else:
         # 4 images
-        xt_list = []
+        img_list = []
         for i in range(images.shape[0]):
-            x_t = resize(images[i], (80, 80))
-            x_t = x_t.astype("float")
-            x_t /= 255.0
-            xt_list.append(x_t)
-        s_t = np.stack((xt_list[0], xt_list[1], xt_list[2], xt_list[3]),
+            img = resize(images[i], (80, 80))
+            img = img.astype("float")
+            img /= 255.0
+            img_list.append(img)
+        new_images = np.stack((img_list[0], img_list[1], img_list[2], img_list[3]),
                        axis=2)
-    s_t = np.expand_dims(s_t, axis=0)
-    return s_t
+    new_images = np.expand_dims(new_images, axis=0)
+    return new_images
 
 
 def get_next_batch(experience, model, num_actions, gamma, batch_size):
@@ -38,31 +38,28 @@ def get_next_batch(experience, model, num_actions, gamma, batch_size):
     X = np.zeros((batch_size, 80, 80, 4))
     Y = np.zeros((batch_size, num_actions))
     for i in range(len(batch)):
-        s_t, a_t, r_t, s_tp1, game_over = batch[i]
-        X[i] = s_t
-        Y[i] = model.predict(s_t)[0]
-        Q_sa = np.max(model.predict(s_tp1)[0])
+        new_images, a_t, r_t, new_imagesp1, game_over = batch[i]
+        X[i] = new_images
+        Y[i] = model.predict(new_images)[0]
+        Q_sa = np.max(model.predict(new_imagesp1)[0])
         if game_over:
             Y[i, a_t] = r_t
         else:
             Y[i, a_t] = r_t + gamma * Q_sa
     return X, Y
 
-
-############################# main ###############################
-
 # initialize parameters
-DATA_DIR = "/Users/kristofreid/Desktop/flapbird_ai"
-NUM_ACTIONS = 2  # number of valid actions (left, stay, right)
+data_path = "/Users/kristofreid/Desktop/flapbird_ai"
+num_actions = 2  # number of valid actions (left, stay, right)
 GAMMA = 0.99  # decay rate of past observations
-INITIAL_EPSILON = 0.1  # starting value of epsilon
-FINAL_EPSILON = 0.0001  # final value of epsilon
-MEMORY_SIZE = 50000  # number of previous transitions to remember
-NUM_EPOCHS_OBSERVE = 100
-NUM_EPOCHS_TRAIN = 2000
+initial_epsilon = 0.1  # starting value of epsilon
+final_epsilon = 0.0001  # final value of epsilon
+mem_size = 50000  # number of previous transitions to remember
+num_epochs_observe = 100
+num_epochs_train = 2000
 
 BATCH_SIZE = 32
-NUM_EPOCHS = NUM_EPOCHS_OBSERVE + NUM_EPOCHS_TRAIN
+num_epochs = num_epochs_observe + num_epochs_train
 
 # build the model
 model = tf.keras.Sequential()
@@ -88,59 +85,59 @@ model.compile(optimizer= "adam", loss="mse")
 
 # train network
 game = flappy_ai.flappy_game()
-experience = collections.deque(maxlen=MEMORY_SIZE)
+experience = collections.deque(maxlen=mem_size)
 
-fout = open(os.path.join(DATA_DIR, "rl-network-results.tsv"), "r")
+fout = open(os.path.join(data_path, "rl-network-results.tsv"), "r")
 num_games, num_wins = 0, 0
-epsilon = INITIAL_EPSILON
-for e in range(NUM_EPOCHS):
+epsilon = initial_epsilon
+for e in range(num_epochs):
     loss = 0.0
     game.reset()
 
     # get first state
     a_0 = 1  # (0 = left, 1 = stay, 2 = right)
-    x_t, r_0, game_over = game.step(a_0)
-    s_t = preprocess_images(x_t)
+    img, r_0, game_over = game.step(a_0)
+    new_images = preprocess_images(img)
 
     while not game_over:
-        s_tm1 = s_t
+        new_imagesm1 = new_images
         # next action
-        if e <= NUM_EPOCHS_OBSERVE:
-            a_t = np.random.randint(low=0, high=NUM_ACTIONS, size=1)[0]
+        if e <= num_epochs_observe:
+            a_t = np.random.randint(low=0, high=num_actions, size=1)[0]
         else:
             if np.random.rand() <= epsilon:
-                a_t = np.random.randint(low=0, high=NUM_ACTIONS, size=1)[0]
+                a_t = np.random.randint(low=0, high=num_actions, size=1)[0]
             else:
-                q = model.predict(s_t)[0]
+                q = model.predict(new_images)[0]
                 a_t = np.argmax(q)
 
         # apply action, get reward
-        x_t, r_t, game_over = game.step(a_t)
-        s_t = preprocess_images(x_t)
+        img, r_t, game_over = game.step(a_t)
+        new_images = preprocess_images(img)
         # if reward, increment num_wins
         if r_t == 1:
             num_wins += 1
         # store experience
-        experience.append((s_tm1, a_t, r_t, s_t, game_over))
+        experience.append((new_imagesm1, a_t, r_t, new_images, game_over))
 
-        if e > NUM_EPOCHS_OBSERVE:
+        if e > num_epochs_observe:
             # finished observing, now start training
             # get next batch
-            X, Y = get_next_batch(experience, model, NUM_ACTIONS,
+            X, Y = get_next_batch(experience, model, num_actions,
                                   GAMMA, BATCH_SIZE)
             loss += model.train_on_batch(X, Y)
 
     # reduce epsilon gradually
-    if epsilon > FINAL_EPSILON:
-        fout = open(os.path.join(DATA_DIR, "rl-network-results.tsv"), "w")
-        epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / NUM_EPOCHS
+    if epsilon > final_epsilon:
+        fout = open(os.path.join(data_path, "rl-network-results.tsv"), "w")
+        epsilon -= (initial_epsilon - final_epsilon) / num_epochs
 
     print("Epoch {:04d}/{:d} | Loss {:.5f} | Win Count: {:d}"
-          .format(e + 1, NUM_EPOCHS, loss, num_wins))
+          .format(e + 1, num_epochs, loss, num_wins))
     fout.write("{:04d}\t{:.5f}\t{:d}\n".format(e + 1, loss, num_wins))
 
     if e % 100 == 0:
-        model.save(os.path.join(DATA_DIR, "rl-network.h5"), overwrite=True)
+        model.save(os.path.join(data_path, "rl-network.h5"), overwrite=True)
 
 fout.close()
-model.save(os.path.join(DATA_DIR, "rl-network.h5"), overwrite=True)
+model.save(os.path.join(data_path, "rl-network.h5"), overwrite=True)
